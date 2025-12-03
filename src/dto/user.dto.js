f6671b63dcb286007377542a1c558e8cc22df1cd
Santiago Sole <santiagosole93@gmail.com>
2025-12-02 21:05:29 -0300

    user dto

---
+++ src/dto/user.dto.js
@@ -0,0 +1,10 @@
+export default class UserDTO {
+    constructor(user) {
+        this.id = user._id;
+        this.first_name = user.first_name;
+        this.last_name = user.last_name;
+        this.email = user.email;
+        this.role = user.role || "user";
+        this.cart = user.cart || null;
+    }
+}
